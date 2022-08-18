90fd297e6decedce034eeaeccc1a1a92215e4f83
piggyu <106943041+piggyu@users.noreply.github.com>
2022-08-19 01:30:33 +0800

    Update index.js

--- flashcards/basic/scripts/index.js
+++ flashcards/basic/scripts/index.js
@@ -1,3 +1,4 @@
+//create flashcard objects which contain questions and answers
 const myFlashcards1 = {
   question: 'What is the color of sky?',
   answer: 'Blue'
@@ -10,9 +11,13 @@ const myFlashcards3 = {
   question: 'Who should you call when there is an emergency?',
   answer: '911'
 };
+
+//put the flashcards objects to myFlashcards array
 let myFlashcards=[myFlashcards1,myFlashcards2,myFlashcards3];
 let questionmart=document.getElementById("questionmart");
 let answermart=document.getElementById("answermart");
+
+//add a listener that shows the answer with one click
 let myFlashcardsindex=0;
 let i=myFlashcardsindex;
 function showanswer(){
@@ -20,6 +25,8 @@ function showanswer(){
     answermart.innerHTML=myFlashcards[i].answer;
   }
 }
+
+//define the functions for the previous and next button to display one question at a time
 function next(){
   if(i>myFlashcards.length-2)
   {i=0;}
